83578c0ab0232125b3fa7cdb0d387162484499f0
Michel van der Burg <m@mbrg.nl>
2025-10-15 17:22:59 +0200

    Test case

--- test/lemonade_stand_test.exs
+++ test/lemonade_stand_test.exs
@@ -2,7 +2,9 @@ defmodule LemonadeStandTest do
   use ExUnit.Case
   doctest LemonadeStand
 
-  test "greets the world" do
-    assert LemonadeStand.hello() == :world
+  test "should return an empty list since the database is empty" do
+    assert Ash.read!(LemonadeStand.Things.Component,
+             load: [suitable: [machine_id: "5ee6d342-c899-4c4f-be16-de99c9173c38"]]
+           ) == []
   end
 end
